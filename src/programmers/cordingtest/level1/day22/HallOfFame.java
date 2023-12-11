eacf8174a6755f21335aec923fcc14c18129ac2c
bokyeongkk <69511656+bokyeongkk@users.noreply.github.com>
2023-12-11 22:43:02 +0900

    Upload day22

--- src/programmers/cordingtest/level1/day22/HallOfFame.java
+++ src/programmers/cordingtest/level1/day22/HallOfFame.java
@@ -46,7 +46,19 @@ public class HallOfFame {
     }
 
     public int[] solution(int k, int[] score) {
-        int[] answer = {};
+        int[] answer = new int[score.length];
+        List<Integer> sc = new ArrayList();
+        for (int i=0; i<score.length; i++) {
+            if (i<k-1){
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i]=sc.get(sc.size()-1);
+            } else if (i>=k-1){
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i]=sc.get(k-1);
+            }
+        }
         return answer;
     }
 }
--- src/programmers/cordingtest/level1/day22/SplitString.java
+++ src/programmers/cordingtest/level1/day22/SplitString.java
@@ -42,6 +42,30 @@ public class SplitString {
 
     public int solution(String s) {
         int answer = 0;
+
+        // 첫번째 문자 추출 (이 글자를 x라고 한다.)
+        char x = s.charAt(0);
+
+        // idx : 처음나온 문자의 개수
+        // idy : 처음나온 문자가 아닌 문자의 개수
+        int idx = 0, idy = 0;
+
+        for (int i=0; i<s.length(); i++) {
+
+            // 처음나온 문자의 갯수와 처음나온 문자가 아닌 문자의 갯수가 동일할 때
+            // 문자열을 분리하기 위해 answer 를 증가 시켜준 후 첫번째 문자를 다시 저장한다.
+            if (idx == idy) {
+                answer++;
+                x = s.charAt(i);
+            }
+
+            // 추출한 첫번째 문자와 동일할 경우 idx 증가
+            // 첫번째 문자가 아닐 경우 idy 증가
+            if (s.charAt(i) == x) idx++;
+            else idy++;
+        }
+
+        // 분해한 문자열의 개수
         return answer;
     }
 }
